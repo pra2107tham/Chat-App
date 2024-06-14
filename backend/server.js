763e84077cda1c321d0c5caa994651b1836a2744
Pratham Shirbhate <prathamshirbhate1909@gmail.com>
2024-06-14 17:00:59 +0530

    Deployment done

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import { app, server } from './scoket/socket.js';
 import dotenv from 'dotenv';
 import cookieParser from 'cookie-parser';
 import express from 'express';
+import path from 'path';
 
 import dbConnect from './db/dbConnect.js';
 
@@ -10,6 +11,8 @@ import messageRoutes from './routes/message.routes.js';
 import userRoutes from './routes/user.routes.js'
 
 const PORT = process.env.PORT || 5000;
+
+const __dirname = path.resolve()
 
 dotenv.config();
 app.use(express.json());
@@ -21,12 +24,11 @@ app.use("/api/messages", messageRoutes)
 app.use("/api/users", userRoutes)
 
 
+app.use(express.static(path.join(__dirname, '/frontend/dist')))
 
-
-
-
-
-
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '/frontend/dist/index.html'))
+})
 
 dbConnect()
 .then(() => {
